adc5d7afa53e1c5ce2b86674034715bd3d7decc2
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-27 20:14:51 +0100

    Fix an issue introduced by bootstrap script when the default qt api cannot be imported

--- qidle/main.py
+++ qidle/main.py
@@ -4,7 +4,8 @@ from .argparser import parse_args
 
 def main():
     args = parse_args()
-    os.environ['QT_API'] = 'pyqt%d' % args.qt
+    if not 'QT_API' in os.environ:
+        os.environ['QT_API'] = 'pyqt%d' % args.qt
     from .app import Application
     app = Application(args.files, args.verbose)
     app.run()
